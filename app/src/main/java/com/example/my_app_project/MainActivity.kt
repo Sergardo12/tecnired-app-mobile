5e6ea19f36144b9f267a8e819a7dab4911b94dab
SGChávezJuárez <129635350+Sergardo12@users.noreply.github.com>
2025-05-24 23:08:58 -0500

    Descomentado del main activity

--- app/src/main/java/com/example/my_app_project/MainActivity.kt
+++ app/src/main/java/com/example/my_app_project/MainActivity.kt
@@ -21,32 +21,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
 
-//        enableEdgeToEdge()
-//        setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
-//
-//        //Boton Registrar
-//        val btnRegistrar = findViewById<Button>(R.id.btnRegistrar)
-//        btnRegistrar.setOnClickListener{
-//            val intento = Intent(this, FormRegistro::class.java)
-//            startActivity(intento)
-//        }
-//
-//        //Boton Iniciar Sesion
-//        val btnIniciarSesion = findViewById<Button>(R.id.btnIniciarSesion)
-//        btnIniciarSesion.setOnClickListener{
-//            val intent = Intent(this, FormInicioSesion::class.java)
-//            startActivity(intent)
-//        }
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //Boton Registrar
+        val btnRegistrar = findViewById<Button>(R.id.btnRegistrar)
+        btnRegistrar.setOnClickListener{
+            val intento = Intent(this, FormRegistro::class.java)
+            startActivity(intento)
+        }
+
+        //Boton Iniciar Sesion
+        val btnIniciarSesion = findViewById<Button>(R.id.btnIniciarSesion)
+        btnIniciarSesion.setOnClickListener{
+            val intent = Intent(this, FormInicioSesion::class.java)
+            startActivity(intent)
+        }
 
 
 
-        startActivity(Intent(this, HomeActivity::class.java))
-        finish()
+//        startActivity(Intent(this, HomeActivity::class.java))
+//        finish()
 
 
     }
