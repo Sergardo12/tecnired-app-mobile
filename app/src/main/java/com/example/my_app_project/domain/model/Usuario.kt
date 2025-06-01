0468a702323cd4a4f4e9d697401d4c761049a4fd
SGChávezJuárez <129635350+Sergardo12@users.noreply.github.com>
2025-06-01 15:35:26 -0500

    Cambios temporales para la prueba de autenticacion de Erick y Diego

--- app/src/main/java/com/example/my_app_project/domain/model/Usuario.kt
+++ app/src/main/java/com/example/my_app_project/domain/model/Usuario.kt
@@ -3,5 +3,14 @@ package com.example.my_app_project.domain.model
 data class Usuario(
     val nombre: String = "",
     val apellido: String = "",
-    val telefono: String = ""
+    val telefono: String = "",
+    val correo: String = "",
+    val profesion: String? = null,
+    val especialidad: String? = null,
+    val descripcion: String? = null,
+    val horario: String? = null,
+    val zonaTrabajo: String? = null,
+    val tarifa: String? = null,
+    val esColaborador : Boolean = false
+
 )
